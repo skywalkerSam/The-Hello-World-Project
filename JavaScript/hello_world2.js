9cbc437a79945abee1439e0774f5d279002af76a
Starboy <samskywalker001@gmail.com>
2022-09-08 13:09:34 +0530

    JavaScript

---
+++ JavaScript/hello_world2.js
@@ -0,0 +1,13 @@
+// "Hello World" program on browsers (Not on node consoles!!!)...
+
+alert("Hello World...")
+
+prompt("Enter Your Name: ")     // Try it...
+
+
+/*
+Opening Developer Tools on browsers...
+
+    >_ F12 : Microsoft Edge (msedge), Chrome, Firefox, Brave, Chromium
+
+*/
